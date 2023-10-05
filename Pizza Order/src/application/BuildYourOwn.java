b4757f72572bcc237145d0a684b03e85fefb567b
stevenlazzara <55415353+stevenlazzara@users.noreply.github.com>
2023-10-05 19:54:37 -0400

    Update BuildYourOwn.java

--- Pizza Order/src/application/BuildYourOwn.java
+++ Pizza Order/src/application/BuildYourOwn.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Build Your Own Pizza Subclass
  * 
- * @author Jonathan Li, Jihun Joo, Bufan Jiang
+ * @author Steven Lazzara
  */
 public class BuildYourOwn extends Pizza {
 
